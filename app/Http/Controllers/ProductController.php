10a0f17395d6ff539ee473884ee3f73bc1cd4f7b
Rizwan Afridi <rizwanafridi@Rizwan-afridi.local>
2019-08-19 14:27:58 +0400

    insert data

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
+use App\Http\Requests\ProductRequest;
+
+ use Symfony\Component\HttpFoundation\Response;
 
 use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
+
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -35,9 +44,21 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        
+        $product = new Product;
+
+        $product->name = $request->name;
+        $product->detail = $request->description;
+        $product->stock = $request->stock;
+        $product->price = $request->price;
+        $product->discount = $request->discount;
+        $product->save();
+
+        return Response([
+        'date' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
     }
 
     /**
